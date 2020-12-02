0694265a887225a5040792478a3efd3504ee8cc6
Kaki Ryan <kakiryan@cs.unc.edu>
2020-12-02 14:09:02 -0500

    Get arguments to primitive operations.

--- src/main/scala/firrtl/traversals/TraverseAdder.scala
+++ src/main/scala/firrtl/traversals/TraverseAdder.scala
@@ -3,7 +3,8 @@
 package traversals
 
 import firrtl.{CircuitState, HighForm, Transform, Utils}
-import firrtl.ir.{DefModule, Expression, Mux, Statement, DefNode, DoPrim, Connect}
+import firrtl.ir.{DefModule, Expression, Mux, Statement, DefNode, DoPrim, Connect, SubField, Reference,
+BundleType}
 import firrtl.Mappers._
 import scala.collection.mutable
 
@@ -31,12 +32,28 @@ class TraverseAdder extends Transform {
   def walkStatement()(s: Statement): Statement = {
     if (s.isInstanceOf[DefNode]) {
       val defNode = s.asInstanceOf[DefNode]
-      println("Node Defn Statement: " +  defNode.name)
+      println("Node Defn Statement: " +  defNode.name + " is initalized with ")
+      print("\t")
+      val value = defNode.value
+      s.map(walkExpression())
     } else if (s.isInstanceOf[Connect]) {
       val connection = s.asInstanceOf[Connect]
-      println("Connection Statement: " + connection.info)
-    }
-    s.map(walkExpression())
+      // the following line will print the lines in the design where the connection is made
+      // println("Connection Statement: " + connection.info)
+      val loc = connection.loc
+      val ref = connection.expr
+      if (loc.isInstanceOf[SubField]) {
+        val flow = loc.asInstanceOf[SubField].flow
+        // println("Connection Flow: " + flow)
+        if (ref.isInstanceOf[Reference]) {
+          println("Connection!")
+          println(ref.asInstanceOf[Reference].name + " assigned to " + loc.asInstanceOf[SubField].name)
+        } 
+      } else {
+        println("Connection location:" + connection.loc)
+      }
+    } 
+ 
     s.map(walkStatement())
   }
 
@@ -45,7 +62,13 @@ class TraverseAdder extends Transform {
     if (e.isInstanceOf[DoPrim]) {
       val doPrim = e.asInstanceOf[DoPrim]
       println("DoPrimitive Operation Expr: " +  doPrim.op + " with " + doPrim.args.length + " arguments.")
-    } else {
+    } else if (e.isInstanceOf[Reference]) {
+      // if (!e.asInstanceOf[Reference].tpe.isInstanceOf[BundleType])
+      print("\t\t")
+      println(e.asInstanceOf[Reference].name)
+    }
+    
+    else {
       // println("Statement: " + s)
     }
     e.map(walkExpression())
